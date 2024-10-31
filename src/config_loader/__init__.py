8aa808d27cc294ec84f4fafe60d119d388e6083e
Leo Cances <leo.cances@solwr.com>
2024-10-31 20:52:19 +0100

    create pypi publish action

--- src/config_loader/__init__.py
+++ src/config_loader/__init__.py
@@ -1,1 +1,8 @@
-__version__ = '0.1.0'
+# src/config_loader/__init__.py
+"""Configuration loader package."""
+
+__version__ = "0.1.0"
+
+from .core.loader import load_config, ConfigurationError
+
+__all__ = ["load_config", "ConfigurationError"]

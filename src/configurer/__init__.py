afd69b13bca09db1177fcfbeaba5f5724b6e954f
Leo Cances <leo.cances@solwr.com>
2024-10-31 22:18:02 +0100

    version 0.2.0

--- src/configurer/__init__.py
+++ src/configurer/__init__.py
@@ -1,7 +1,7 @@
 # src/configurer/__init__.py
 """Configuration loader package."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from configurer.core.loader import load_config, ConfigurationError
 
